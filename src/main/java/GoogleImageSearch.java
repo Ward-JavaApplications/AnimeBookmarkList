99f7afa53f3c31173266ca1a0fee1fc1335b90bc
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2021-07-12 16:47:56 +0200

    some trials with the image load idea

--- src/main/java/GoogleImageSearch.java
+++ src/main/java/GoogleImageSearch.java
@@ -1,12 +1,23 @@
+import org.apache.poi.sl.draw.Drawable;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class GoogleImageSearch {
     GoogleImageSearch(){
-        googleSearch("");
-        System.out.println(getImage("Naruto,Wikipedia,Cover"));
+        ownTry();
+        //w3spoint();
+        //googleSearch("");
+        //System.out.println(getImage("Naruto,Wikipedia,Cover"));
     }
     private void googleSearch(String keywords){
         try {
@@ -38,6 +49,49 @@ public class GoogleImageSearch {
         }
         return result;
     }
+    private boolean displayImage(String imageSrc){
+        ImagePanel imagePanel = new ImagePanel(imageSrc);
+        return imagePanel.isImageDrawnSuccesfully();
+    }
 
+    public void ownTry(){
+        try {
+            String url = "https://www.google.com/search?q=naruto&sxsrf=ALeKk03jnsnO4iM6nvZ2IY55vQQhIKJa9w:1625854393995&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjc1vDMy9bxAhWH6aQKHcR2ClsQ_AUoAXoECAEQAw&biw=1278&bih=1286";
+            Document doc = Jsoup.connect(url).get();
+            Elements images = doc.select("img[src]");
+            for(Element e:images){
+                System.out.print(e.attr("src"));
+                if(displayImage(e.attr("abs:href"))) return;
+
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    private void w3spoint(){
+        Document document;
+        String url = "https://www.wikipedia.org/";
+        try {
+            //Get Document object after parsing the html from given url.
+            document = Jsoup.connect(url).get();
+
+            //Get images from document object.
+            Elements images =
+                    document.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
+
+            //Iterate images and print image attributes.
+            for (Element image : images) {
+                System.out.println("Image Source: " + image.attr("src"));
+                System.out.println("Image Height: " + image.attr("height"));
+                System.out.println("Image Width: " + image.attr("width"));
+                System.out.println("Image Alt Text: " + image.attr("alt"));
+                System.out.println("");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
